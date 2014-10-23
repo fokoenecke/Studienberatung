f0794b9e4a11007d51b4ec8a248b9b4696fe2ebf
fokoenecke <finnole.koenecke@haw-hamburg.de>
2014-10-23 10:56:21 +0200

    Studienberatung mit Frage

---
+++ Studienberatung.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Studienberatung{
+	public static void main(String[]args){
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Hast du Abitur?");
+		String input = scanner.nextLine();
+		System.out.println(input);
+	}
+}
